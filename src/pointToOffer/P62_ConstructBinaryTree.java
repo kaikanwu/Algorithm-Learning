9d6fd6529780060692155cddeca37e62505774b0
kaikanwu <wukaikan@gmail.com>
2018-10-26 21:20:30 +0800

    update

--- src/pointToOffer/P62_ConstructBinaryTree.java
+++ src/pointToOffer/P62_ConstructBinaryTree.java
@@ -1,8 +1,100 @@
 package pointToOffer;
 
+import com.sun.org.apache.regexp.internal.RE;
+import jdk.nashorn.internal.ir.IdentNode;
+import structure.TreeNode;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
+ *  7. 重建二叉树
+ *
+ *  题目：输入某二叉树的前序遍历和中序遍历的结果，请重建该二叉树。
+ *  假设输的前序遍历和中序遍历的结果中不含重复的数字。
+ *
+ *  解题思路：
+ *  1. 叉树中，需要前序+中序，或者中序+后序 才能重建一个二叉树。 前序+后序 无法重建。
+ *
+ *
  * @author kaikanwu
  * @date 25/10/2018
  */
 public class P62_ConstructBinaryTree {
+
+
+    /**
+     *  方法 1
+     * @param preOrder
+     * @param inOrder
+     * @return
+     */
+    public static TreeNode construct(int[] preOrder, int[] inOrder) {
+        if (preOrder == null || inOrder == null || preOrder.length == 0 || preOrder.length != inOrder.length) {
+            return  null;
+        }
+
+        return constructCore(preOrder, 0, inOrder, 0, preOrder.length);
+    }
+
+
+    public static TreeNode constructCore(int[] preOrder, int preOrderStart, int[] inOrder, int inOrderStart, int length) {
+
+        if (length == 0) {
+            return null;
+        }
+
+        int inOrderIndex = -1;
+        for (int i = inOrderStart; i < inOrderStart + length; i++) {
+            if (inOrder[i] == preOrder[preOrderStart]) {
+                inOrderIndex = i;
+                break;
+            }
+        }
+
+        int leftLength = inOrderIndex - inOrderStart;
+
+        TreeNode node = new TreeNode(preOrder[preOrderStart]);
+        node.left = constructCore(preOrder, preOrderStart + 1, inOrder, inOrderStart, leftLength);
+        node.right = constructCore(preOrder, preOrderStart + leftLength + 1, inOrder, inOrderIndex + 1, length - leftLength - 1);
+        return node;
+    }
+
+    /**
+     * 方法 2
+     *
+     * @param pre
+     * @param ints
+     * @return
+     */
+
+    private Map<Integer, Integer> indexForInOrders = new HashMap<>();
+//    public TreeNode reConstructBinaryTree(int[] pre, int[] in) {
+//
+//
+//
+//
+//
+//    }
+
+
+    public static void main(String[] args) {
+
+
+        int[] pre = {1, 2, 3, 5, 3};
+        int[] in = {4, 2, 5, 1, 3};
+
+        TreeNode root = construct(pre, in);
+
+
+        List<Integer> preOrder = P60_TraverseOfBinaryTree.preOrderIteratively(root);
+        List<Integer> inOrder = P60_TraverseOfBinaryTree.inOrderIteratively(root);
+        List<Integer> postOrder = P60_TraverseOfBinaryTree.postOrderIteratively(root);
+
+        System.out.println(preOrder);
+        System.out.println(inOrder);
+        System.out.println(postOrder);
+    }
+
 }
