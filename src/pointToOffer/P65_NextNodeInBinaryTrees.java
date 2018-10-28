e0c5e54ecc26f5db32c096c6bb640c77046ccf8c
kaikanwu <wukaikan@gmail.com>
2018-10-28 22:27:31 +0800

    update

---
+++ src/pointToOffer/P65_NextNodeInBinaryTrees.java
@@ -0,0 +1,14 @@
+package pointToOffer;
+
+/**
+ * Q.给定一个二叉树和其中的一个节点，如何找出中序遍历的下一个节点？
+ *   树中的节点除了有两个分别指向左、右子节点的指针，还有一个指向父节点的指针。
+ *
+ * A.
+ * 1. 中序遍历： 左根右
+ *
+ * @author kaikanwu
+ * @date 28/10/2018
+ */
+public class P65_NextNodeInBinaryTrees {
+}
