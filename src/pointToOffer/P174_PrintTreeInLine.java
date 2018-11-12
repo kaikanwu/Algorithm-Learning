8e990f4b4a15e78f9d92402efa7949c9581af70a
kaikanwu <wukaikan@gmail.com>
2018-11-12 11:57:55 +0800

    update

--- src/pointToOffer/P174_PrintTreeInLine.java
+++ src/pointToOffer/P174_PrintTreeInLine.java
@@ -7,60 +7,45 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 /**
- *  Q: 从上到下打印二叉树
- *  从上到下打印出二叉树的每个节点，同一层节点按照从左到右的顺序打印
+ *  Q: 分行从上到下打印二叉树
+ *  从上到下按层打印二叉树，同一层的节点按从左到右的顺序打印，
+ *  每一层打印到一行。
  *
  *
- *  A：
- *  这是一个遍历题
- *  level-order
- *
- *  1. 首先熟悉队列 queue 的操作
- *  insert -- add(e)  -  offer(e)
- *  remove -- remove() - poll() 返回队列的第一个元素，并删除
- *  examine -- element() - peek()
- *  优先考虑右侧的方法，右侧的方法返回值不存在时返回 null
- *
- *  思路：使用队列来进行层次遍历
  *
  *
  * @author kaikanwu
  * @date 12/11/2018
  */
-public class P171_PrintTreeFromTopToBottom {
+public class P174_PrintTreeInLine {
 
-    public ArrayList<Integer> PrintFromTopToBottom(TreeNode<Integer> root) {
+    public static ArrayList<ArrayList<Integer>> printTreeInLine(TreeNode<Integer> root) {
 
-        if (root == null) {
-            return null;
-        }
-
+        ArrayList<ArrayList<Integer>> result = new ArrayList<>();
         Queue<TreeNode<Integer>> queue = new LinkedList<>();
-        ArrayList<Integer> result = new ArrayList<>();
 
         queue.offer(root);
         while (!queue.isEmpty()) {
+            ArrayList<Integer> list = new ArrayList<>();
 
             for (int qSize = queue.size(); qSize > 0; qSize--) {
 
                 TreeNode<Integer> t = queue.poll();
-
-                // 如果队列的第一个数为空 则跳出循环
                 if (t == null) {
                     continue;
                 }
 
-                result.add(t.val);
+                list.add(t.val);
                 queue.offer(t.left);
                 queue.offer(t.right);
             }
+            if (list.size() != 0) {
+                result.add(list);
+            }
         }
 
-
         return result;
-
     }
-
 
 
     public static void main(String[] args) {
@@ -72,12 +57,7 @@ public class P171_PrintTreeFromTopToBottom {
         root.right.left = new TreeNode<>(6);
         root.right.right = new TreeNode<>(7);
 
-        P171_PrintTreeFromTopToBottom print = new P171_PrintTreeFromTopToBottom();
-        System.out.println(print.PrintFromTopToBottom(root));
+        System.out.println(printTreeInLine(root));
 
     }
-
-
-
-
 }
---
+++ src/pointToOffer/P176_PrintTreeInSpecial.java
@@ -0,0 +1,77 @@
+package pointToOffer;
+
+import org.omg.PortableInterceptor.INACTIVE;
+import structure.TreeNode;
+
+import java.util.*;
+
+/**
+ *  Q:  之字形打印二叉树
+ *  请实现一个函数按照之字形顺序打印二叉树，即第一行按照从左到右打印，
+ *  第二行按照从右到左打印，第三行再按照从左到右打印，以此类推。
+ *
+ * @author kaikanwu
+ * @date 12/11/2018
+ */
+public class P176_PrintTreeInSpecial {
+
+    public static ArrayList<ArrayList<Integer>> print(TreeNode<Integer> root) {
+
+        ArrayList<ArrayList<Integer>> result = new ArrayList<>();
+        Queue<TreeNode<Integer>> queue = new LinkedList<>();
+
+        queue.offer(root);
+        boolean reverse = false;
+
+        while (!queue.isEmpty()) {
+
+            ArrayList<Integer> list = new ArrayList<>();
+            for (int qSize = queue.size(); qSize > 0; qSize--) {
+
+                TreeNode<Integer> t = queue.poll();
+                if (t == null) {
+                    continue;
+                }
+
+                list.add(t.val);
+                queue.offer(t.left);
+                queue.offer(t.right);
+            }
+            if (reverse) {
+                Collections.reverse(list);
+            }
+            // 将下一轮反转
+            reverse = !reverse;
+            if (list.size() != 0) {
+                result.add(list);
+            }
+
+        }
+        return result;
+    }
+
+
+
+    public static void main(String[] args) {
+        TreeNode<Integer> root = new TreeNode<>(1);
+        root.left = new TreeNode<>(2);
+        root.right = new TreeNode<>(3);
+        root.left.left = new TreeNode<>(4);
+        root.left.right = new TreeNode<>(5);
+        root.right.left = new TreeNode<>(6);
+        root.right.right = new TreeNode<>(7);
+
+        for (ArrayList<Integer> arr:print(root)
+             ) {
+
+            System.out.println(arr);
+
+        }
+     
+
+    }
+
+
+
+
+}
