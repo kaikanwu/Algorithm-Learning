dba59eb315bf8082f7a49426a8402942df8f923e
kaikanwu <wukaikan@gmail.com>
2018-12-07 23:52:43 +0800

    update

---
+++ src/leetcode/Q349_IntersectionOfTwoArrays.java
@@ -0,0 +1,69 @@
+package leetcode;
+
+import java.util.*;
+
+
+/**
+ *  Q:  给定两个数组，编写一个函数来计算它们的交集。
+ *
+ *      示例 1:
+ *      输入: nums1 = [1,2,2,1], nums2 = [2,2]
+ *      输出: [2]
+ *
+ *      示例 2:
+ *      输入: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+ *      输出: [9,4]
+ *
+ *      说明:
+ *      输出结果中的每个元素一定是唯一的。
+ *      我们可以不考虑输出结果的顺序。
+ * @author kaikanwu
+ * @date 07/12/2018
+ */
+public class Q349_IntersectionOfTwoArrays {
+
+    public int[] intersection(int[] nums1, int[] nums2) {
+
+
+        Map<Integer, Integer> map = new HashMap<>(nums1.length+nums2.length);
+
+        for (int i :nums1) {
+
+            if (map.containsKey(i)) {
+                map.put(i, map.get(i) + 1);
+            }
+            else {
+                map.put(i,  1);
+            }
+
+
+        }
+        for (int i :nums2) {
+
+            if (map.containsKey(i)) {
+                map.put(i, map.get(i) + 1);
+            }
+            else {
+                map.put(i, 1);
+            }
+
+        }
+
+        Set<Map.Entry<Integer, Integer>> set = map.entrySet();
+
+        int[] result = new int[nums1.length + nums2.length];
+        int i = 0;
+
+        for (Map.Entry<Integer,Integer> entry :set) {
+            if (entry.getValue() > 1) {
+                result[i] = entry.getKey();
+                i++;
+            }
+        }
+        return result;
+    }
+
+
+
+    
+}
