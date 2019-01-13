535c8d5399ea47083601ecfeffa44afddd7f13b1
kaikanwu <wukaikan@gmail.com>
2019-01-13 19:52:58 +0800

    add Q206

---
+++ src/leetcode/Q206_ReverseLinkedList.java
@@ -0,0 +1,46 @@
+package leetcode;
+
+/**
+ *  Q:  反转一个单链表。
+ *
+ * @author kaikanwu
+ * @date 13/01/2019
+ */
+public class Q206_ReverseLinkedList {
+
+
+    /**
+     * Single Linked List
+     */
+    public class ListNode{
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+
+    /**
+     * Time:O(n)
+     * Space: O(1)
+     * @param head
+     * @return
+     */
+    public ListNode reverseList(ListNode head) {
+
+        ListNode cur = head;
+        ListNode pre = null;
+        // 当 cur == null 时， pre 就指向反转后链表的头节点
+        while (cur != null) {
+            ListNode next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+
+        return pre;
+    }
+
+}
