7192d612e127b6b873771220ec0d37f591d8acea
kaikanwu <wukaikan@gmail.com>
2019-01-13 20:42:24 +0800

    update

--- src/leetcode/Q88_MergeSortedArray.java
+++ src/leetcode/Q88_MergeSortedArray.java
@@ -1,8 +1,13 @@
 package leetcode;
 
 /**
- *  给定两个有序整数数组 nums1 和 nums2，
+ *  Q:给定两个有序整数数组 nums1 和 nums2，
  *  将 nums2 合并到 nums1 中，使得 num1 成为一个有序数组。
+ *
+ *  说明: 初始化 nums1 和 nums2 的元素数量分别为 m 和 n。
+ *      你可以假设 nums1 有足够的空间（空间大小大于或等于 m + n）来保存 nums2 中的元素。
+ *
+ *  A:  使用双指针来解决。可以关联复习 Merge Sort。
  *
  * @author kaikanwu
  * @date 26/11/2018
@@ -12,10 +17,8 @@ public class Q88_MergeSortedArray {
      *  其实就是归并排序里的 merge 两个排好序的数组的操作
      *  这里的 -- 就是从大往小的排列。
      *  是最优解！使用的变量也最少！
-     * @param nums1
-     * @param m
-     * @param nums2
-     * @param n
+     *
+     *  Time: O(m+n), Space: O(1)
      */
     public void merge(int[] nums1, int m, int[] nums2, int n) {
 
@@ -24,6 +27,7 @@ public class Q88_MergeSortedArray {
         int j = n - 1;
         int index = m + n - 1;
 
+        // 注意这里的判断条件
         while (i >= 0 || j >= 0) {
 
             if (i < 0) {
