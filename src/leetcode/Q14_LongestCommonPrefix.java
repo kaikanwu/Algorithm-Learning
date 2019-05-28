9b6f80f3cf0bbcae85c8ed028e73a05143e605cc
kaikanwu <wukaikan@gmail.com>
2019-05-28 10:42:15 +0800

    update Q14

---
+++ src/leetcode/Q14_LongestCommonPrefix.java
@@ -0,0 +1,43 @@
+package leetcode;
+
+/**
+ * 编写一个函数来查找字符串数组中的最长公共前缀。
+ * 如果不存在公共前缀，返回空字符串 ""。
+ *
+ * 示例 1:
+ * 输入: ["flower","flow","flight"]
+ * 输出: "fl"
+ *
+ * 示例 2:
+ * 输入: ["dog","racecar","car"]
+ * 输出: ""
+ * 解释: 输入不存在公共前缀。
+ * 说明:
+ * 所有输入只包含小写字母 a-z 。
+ *
+ *  A: LCP(S 1 ​…S n ​ )= LCP(LCP(LCP(S 1 ​ ,S 2 ​ ),S 3 ​ ),…S n ​ )
+ *
+ * @author kaikanwu
+ * @date 5/28/2019
+ */
+public class Q14_LongestCommonPrefix {
+
+    public String longestCommonPrefix(String[] strs) {
+
+        if (strs.length == 0) {
+            return "";
+        }
+
+        String prefix = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if (prefix.isEmpty()) {
+                    return "";
+                }
+            }
+        }
+
+        return prefix;
+    }
+}
