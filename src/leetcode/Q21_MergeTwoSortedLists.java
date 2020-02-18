df89e67ea19f138626106b8b246f2d000203e61e
kaikanwu <wukaikan@gmail.com>
2020-02-18 15:13:50 +0800

    update q21

--- src/leetcode/Q21_MergeTwoSortedLists.java
+++ src/leetcode/Q21_MergeTwoSortedLists.java
@@ -3,6 +3,13 @@ package leetcode;
 import structure.ListNode;
 
 /**
+ * 将两个有序链表合并为一个新的有序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+ *
+ * 示例：
+ *
+ * 输入：1->2->4, 1->3->4
+ * 输出：1->1->2->3->4->4
+ *
  * @author kaikanwu
  * @date 10/02/2020
  */
@@ -46,12 +53,14 @@ public class Q21_MergeTwoSortedLists {
             p = p.next;
         }
 
+        // 退出上面的 while 循环，说明有一个链表的上的节点已经全部对比完
         if (l1 != null) {
             p.next = l1;
         }
         if (l2 != null) {
             p.next = l2;
         }
+        // 首节点是从 p.next 开始的 即 dummy.next
         return dummy.next;
 
     }
