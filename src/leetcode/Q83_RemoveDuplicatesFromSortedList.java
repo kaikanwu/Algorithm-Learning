33b06ac9a24927b9e1ec413b8ba20c88464e8726
kaikanwu <wukaikan@gmail.com>
2020-02-19 11:14:13 +0800

    update q83

---
+++ src/leetcode/Q83_RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,52 @@
+package leetcode;
+
+import structure.ListNode;
+
+/**
+ * 给定一个排序链表，删除所有重复的元素，使得每个元素只出现一次。
+ *
+ * 示例 1:
+ * 输入: 1->1->2
+ * 输出: 1->2
+ *
+ * 示例 2:
+ * 输入: 1->1->2->3->3
+ * 输出: 1->2->3
+ *
+ * @author kaikanwu
+ * @date 19/02/2020
+ */
+public class Q83_RemoveDuplicatesFromSortedList {
+
+
+    /**
+     * Time:O(n) Space:O(1)
+     */
+    public ListNode removeDuplicates(ListNode head) {
+
+        // 处理空链表的情况
+        if (head == null) {
+            return null;
+        }
+
+        ListNode p = head;
+        ListNode n = head.next;
+
+
+        while (n != null) {
+
+            if (p.val == n.val) {
+                p.next = n.next;
+            }else {
+                p = p.next;
+            }
+            n = n.next;
+
+        }
+        return head;
+
+    }
+
+
+
+}
