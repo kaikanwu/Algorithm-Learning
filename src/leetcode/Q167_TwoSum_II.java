fb32136d241939f99fcfda4666061a2bbfde6ea7
kaikanwu <wukaikan@gmail.com>
2020-05-03 10:53:02 +0800

    update two questions

--- src/leetcode/Q167_TwoSum_II.java
+++ src/leetcode/Q167_TwoSum_II.java
@@ -1,18 +1,18 @@
 package leetcode;
 
 /**
- *  Q:  给定一个已按照升序排列 的有序数组，找到两个数使得它们相加之和等于目标数。
- *      函数应该返回这两个下标值 index1 和 index2，其中 index1 必须小于 index2。
- *      说明:
- *      返回的下标值（index1 和 index2）不是从零开始的。
- *      你可以假设每个输入只对应唯一的答案，而且你不可以重复使用相同的元素。
- *
- *      示例:
- *      输入: numbers = [2, 7, 11, 15], target = 9
- *      输出: [1,2]
- *      解释: 2 与 7 之和等于目标数 9 。因此 index1 = 1, index2 = 2 。
- *
- *   A: 双指针
+ * Q:  给定一个已按照升序排列 的有序数组，找到两个数使得它们相加之和等于目标数。
+ * 函数应该返回这两个下标值 index1 和 index2，其中 index1 必须小于 index2。
+ * 说明:
+ * 返回的下标值（index1 和 index2）不是从零开始的。
+ * 你可以假设每个输入只对应唯一的答案，而且你不可以重复使用相同的元素。
+ * <p>
+ * 示例:
+ * 输入: numbers = [2, 7, 11, 15], target = 9
+ * 输出: [1,2]
+ * 解释: 2 与 7 之和等于目标数 9 。因此 index1 = 1, index2 = 2 。
+ * <p>
+ * A: 双指针
  *
  * @author kaikanwu
  * @date 24/11/2018
@@ -26,7 +26,7 @@ public class Q167_TwoSum_II {
     public int[] twoSum(int[] numbers, int target) {
 
         int i = 0;
-        int j = numbers.length-1;
+        int j = numbers.length - 1;
         int[] result = new int[2];
 
         // 不能使用重复的元素，所以条件为 i<j
@@ -35,15 +35,13 @@ public class Q167_TwoSum_II {
             int sum = numbers[i] + numbers[j];
             if (sum == target) {
                 // 根据题意，返回的下标不是从0开始
-                result[0] = i+1;
-                result[1] = j+1;
+                result[0] = i + 1;
+                result[1] = j + 1;
                 return result;
-            }
-            else if (sum < target) {
+            } else if (sum < target) {
                 // 因为数组是升序，当目前的 sum 小于 target 时，只需要让 i + 1
                 i++;
-            }
-            else {
+            } else {
                 j--;
             }
         }
@@ -53,12 +51,12 @@ public class Q167_TwoSum_II {
 
     public static void main(String[] args) {
 
-        int[] arr = {2,7,11,15};
+        int[] arr = {2, 7, 11, 15};
 
         Q167_TwoSum_II t = new Q167_TwoSum_II();
         int[] result = t.twoSum(arr, 9);
-        for (int i: result
-             ) {
+        for (int i : result
+        ) {
             System.out.println(i);
         }
     }
--- src/leetcode/Q633_SumOfSquareNumbers.java
+++ src/leetcode/Q633_SumOfSquareNumbers.java
@@ -1,37 +1,36 @@
 package leetcode;
 
 /**
- *  给定一个非负整数 c ，你要判断是否存在两个整数 a 和 b，使得 a2 + b2 = c。
+ * 给定一个非负整数 c ，你要判断是否存在两个非负整数 a 和 b，使得 a^2 + b^2 = c。
  *
  * @author kaikanwu
  * @date 24/11/2018
  */
 public class Q633_SumOfSquareNumbers {
 
-    public boolean judgeSquareSum(int c) {
+
+    /**
+     * Time: O(N)，实际上是 O(sqrt(target))，sqrt = square root
+     * Space: O(1)
+     */
+    public boolean judgeSquareSum(int target) {
 
         int i = 0;
-        int j = (int)Math.sqrt(c);
+        // 注意这里对结果的右边界做了剪枝处理
+        int j = (int) Math.sqrt(target);
 
         while (i <= j) {
 
-            int sum = i*i + j*j;
+            int sum = i * i + j * j;
 
-            if (sum == c) {
+            if (sum == target) {
                 return true;
-            }
-            else if (sum< c){
+            } else if (sum < target) {
                 i++;
-            }
-
-            else {
+            } else {
                 j--;
             }
         }
         return false;
-
     }
-
-
-
 }
