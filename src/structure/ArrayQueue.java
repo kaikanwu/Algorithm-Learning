818a0dd2b3c77e54f3fbb3f56a2e32654f5eb7c4
kaikanwu <wukaikan@gmail.com>
2020-05-17 15:43:02 +0800

    update

--- src/structure/ArrayQueue.java
+++ src/structure/ArrayQueue.java
@@ -27,10 +27,14 @@ public class ArrayQueue {
 
     /**
      * add a new item
+     *
+     * A problem here: with a serious of enqueue and dequeue, the index of head and tail will move the last place of the queue,
+     * at that time, this queue cannot add new items, even the queue is empty.
+     *
      * @param item the new element
      * @return true or false
      */
-    public boolean enqueue(String item) {
+    public boolean enqueue1(String item) {
         if (tail == n) {
             return false;
         }
@@ -38,6 +42,31 @@ public class ArrayQueue {
         tail++;
         return true;
     }
+
+
+    /**
+     * Try to solve the problem of enqueue1:
+     * copy the array, let it  start from the first place of an arry
+     */
+    public boolean enqueue2(String item) {
+        if (tail == n) {
+            if (head == 0) {
+                return false;
+            }
+          // manual copy the array
+            for (int i = head; i < tail; i++) {
+                items[i - head] = items[i];
+            }
+            // reassign the index
+            tail -= head;
+            head = 0;
+
+        }
+        items[tail] = item;
+        tail++;
+        return true;
+    }
+
 
     /**
      * pop an element
