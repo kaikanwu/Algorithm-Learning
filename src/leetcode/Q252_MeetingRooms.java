39ca0a03d26bed8224e394fb7b6fd34d3d7f53dc
kaikanwu <wukaikan@gmail.com>
2020-06-27 20:48:45 +0800

    update

--- src/leetcode/Q252_MeetingRooms.java
+++ src/leetcode/Q252_MeetingRooms.java
@@ -22,12 +22,15 @@ public class Q252_MeetingRooms {
 
 
     /**
-     * Time:  O(n*log(n))
+     * Time:  O(n*log(n))   (actually: O(n*log(n)) + O(n) )
      * Space: O(1)
      */
     public boolean canAttendMeetings(List<Interval> intervals) {
+        // 1. 先根据区间的「开始时间」对数据进行排序，这里可以用 jdk 1.8 提供的  List.sort() 方法
+        // （这里先花费 n*log(n) 的时间进行排序）
         intervals.sort((a, b) -> a.start - b.start);
 
+        // 2. （这里再花费 O(n) 的时间去对比相邻的区间）
         for (int i = 0; i < intervals.size() - 1; i++) {
             if (intervals.get(i).end > intervals.get(i + 1).start) {
                 return false;
