c29a11c3914abaeda3918ff57420faeefd3d4a2c
kaikanwu <wukaikan@gmail.com>
2020-07-12 10:59:59 +0800

    update

--- src/leetcode/Q518_CoinChangeII.java
+++ src/leetcode/Q518_CoinChangeII.java
@@ -28,6 +28,32 @@ public class Q518_CoinChangeII {
 
 
 
+    
+
+    public int change2(int amount, int[] coins) {
+
+        if (coins == null) {
+            return 0;
+        }
+
+        // 注意这里初始化后一个数组后，值都是 0
+        int[] dp = new int[amount + 1];
+        dp[0] = 1;
+
+        for (int coin : coins) {
+
+            for (int i = coin; i <= amount; i++) {
+
+                dp[i] += dp[i - coin];
+
+            }
+
+        }
+        return dp[amount];
+    }
+
+
+
 
 
 }
