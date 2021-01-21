b4afe2712634b67c8fb0e722bc3b4653a01b8427
kaikanwu <wukaikan@gmail.com>
2021-01-21 23:46:41 +0800

    update

--- src/leetcode/Q104_MaxDepth.java
+++ src/leetcode/Q104_MaxDepth.java
@@ -11,8 +11,11 @@ public class Q104_MaxDepth {
     public int maxDepth(TreeNode root) {
         if (root == null) {
             return 0;
+        }else{
+            int left = maxDepth(root.left);
+            int right = maxDepth(root.right);
+            return Math.max(left, right) + 1;
         }
-        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
     }
 
 }
