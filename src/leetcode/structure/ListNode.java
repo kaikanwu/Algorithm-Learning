35828a55019084384f5fad6c0a61c63c6600075e
kaikanwu <wukaikan@gmail.com>
2021-04-02 22:36:07 +0800

    update q6

--- src/leetcode/structure/ListNode.java
+++ src/leetcode/structure/ListNode.java
@@ -10,11 +10,11 @@ public class ListNode {
 
     }
 
-    ListNode(int val) {
+    public ListNode(int val) {
         this.val = val;
     }
 
-    ListNode(int val, ListNode next) {
+    public ListNode(int val, ListNode next) {
         this.val = val;
         this.next = next;
     }
---
+++ src/pointToOfferII/Q6_ReversePrint.java
@@ -0,0 +1,37 @@
+package pointToOfferII;
+
+import leetcode.structure.ListNode;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+/**
+ * 输入一个链表的头节点，从尾到头反过来返回每个节点的值（用数组返回）。
+ * 示例 1：
+ * 输入：head = [1,3,2]
+ * 输出：[2,3,1]
+ */
+public class Q6_ReversePrint {
+
+    public static int[] reversePrint(ListNode head) {
+        Stack<Integer> stack = new Stack<>();
+        while (head != null) {
+            stack.push(head.val);
+            head = head.next;
+        }
+        int[] result = new int[stack.size()];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = stack.pop();
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(3);
+        head.next.next = new ListNode(2);
+
+        System.out.println(Arrays.toString(reversePrint(head)));
+    }
+}
